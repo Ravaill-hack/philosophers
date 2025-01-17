fd3927cd711a8342c8b078cba5c955c0f1f1a7dd
lmatkows <lmatkows@student.42perpignan.fr>
2025-01-17 15:32:16 +0100

    rjirjgirji

--- philo/actions.c
+++ philo/actions.c
@@ -6,7 +6,7 @@
 /*   By: lmatkows <lmatkows@student.42perpignan.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/13 18:52:11 by lmatkows          #+#    #+#             */
-/*   Updated: 2025/01/17 15:23:47 by lmatkows         ###   ########.fr       */
+/*   Updated: 2025/01/17 15:31:48 by lmatkows         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,11 +23,11 @@ void	*ft_do_sth(void *var)
 int	ft_take_2_forks(t_var *var, int i_p)
 {
 	if (pthread_mutex_lock((var->philo[i_p]).f_lft) == 0)
-		ft_put_message(i_p, var, "has taken a fork");
+		ft_put_message(i_p, var, "has taken a fork\n");
 	else
 		return (0);
 	if (pthread_mutex_lock((var->philo[i_p]).f_rgt) == 0)
-		ft_put_message(i_p, var, "has taken a fork");
+		ft_put_message(i_p, var, "has taken a fork\n");
 	else
 		return (0);
 	return (1);
@@ -41,5 +41,10 @@ void	ft_free_2_forks(t_var *var, int i_p)
 
 void	ft_is_thinking(t_var *var, int i_p)
 {
-	ft_put_message(i_p, var, "has taken a fork");
+	ft_put_message(i_p, var, "is thinking\n");
+}
+
+void	ft_is_sleeping(t_var *var, int i_p)
+{
+	ft_put_message(i_p, var, "is sleeping\n");
 }
